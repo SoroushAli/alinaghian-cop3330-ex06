e704802399c0d2e11e4c2b6206e256b89b4c8bc8
SoroushAli <84461814+SoroushAli@users.noreply.github.com>
2021-06-06 05:23:05 -0400

    Solution for Exercise 6

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -3,6 +3,11 @@
  *  Copyright 2021 Soroush Alinaghian
  */
 package base;
+
+import java.time.Year;
+import java.util.Calendar;
+import java.util.Scanner;
+
 /*
 Example Output
 
@@ -21,7 +26,46 @@ Challenge
 Handle situations where the program returns a negative number by stating that the user can already retire.
  */
 public class App {
+    static Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
+
+
+        String firstInput = readAge();
+        String secondInput = readAge();
+        int currentAge = getAge(firstInput);
+        int retireAge = getAge(secondInput);
+        int yearsLeft = remainingYears(retireAge, currentAge);
+        int currentYear = findYear();
+        int retireYear = findRetireYear(currentYear, yearsLeft);
+        generateOutput(currentAge, retireAge, yearsLeft, currentYear, retireYear);
+
+    }
+
+    private static int findRetireYear(int currentYear, int yearsLeft) {
+        return currentYear + yearsLeft;
+    }
+
+    private static int findYear() {
+        return Calendar.getInstance().get(Calendar.YEAR);
+    }
+
+    private static void generateOutput(int currentAge, int retireAge, int yearsLeft, int currentYear, int retireYear) {
+        System.out.println("What is your current age? " + currentAge +
+                "\nAt what age would you like to retire? " + retireAge +
+                "\nYou have " + yearsLeft + " years left until you can retire." +
+                "\nIt's " + currentYear + ", so you can retire in " + retireYear + ".");
+    }
+
+    private static int remainingYears(int retireAge, int currentAge) {
+        return retireAge - currentAge;
+    }
+
+    private static int getAge(String input) {
+        return Integer.parseInt(input);
+    }
+
+    private static String readAge() {
+        return in.next();
     }
 }
