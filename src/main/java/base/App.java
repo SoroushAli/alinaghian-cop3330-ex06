16fbdeca7a06d816d89dd19e53536d0ba35db430
SoroushAli <84461814+SoroushAli@users.noreply.github.com>
2021-06-06 04:36:04 -0400

    Initial commit

---
+++ src/main/java/base/App.java
@@ -0,0 +1,27 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Soroush Alinaghian
+ */
+package base;
+/*
+Example Output
+
+What is your current age? 25
+At what age would you like to retire? 65
+You have 40 years left until you can retire.
+It's 2015, so you can retire in 2055.
+
+Constraints
+
+Again, be sure to convert the input to numerical data before doing any math.
+Don’t hard-code the current year into your program. Get it from the system time via your programming language.
+
+Challenge
+
+Handle situations where the program returns a negative number by stating that the user can already retire.
+ */
+public class App {
+    public static void main(String[] args) {
+        
+    }
+}
